8ffb0d5e2fc9ad506514eaed9e5e177da37698ea
Kaiynatm57 <Kaiynatm57@gmail.com>
2026-03-05 10:41:52 +0500

    Add del utf and it working also add AC working

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
+let FirstInput="";
+let SecondInput="";
+let op="";
+
 const display = document.getElementById('display');
 function appendToDisplay(input) {
     display.value += input;
+   
 }
 
 function clearDisplay() {
-    display.value = "";
+    document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+       
+    
 }
+//AC button working
+function allclear(){
+    display.value="0"; 
+}
+
 function calculate() {
     try {
+      
         display.value = eval(display.value);
+        
     } catch (error) {
+       
         display.value = 'Error';
     }
 }
